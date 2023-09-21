fc8ffdd28ab92e9196d9bcc47f1cb3d28e7c6108
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-09-21 08:46:16 -0300

    First activity main

--- app/src/main/java/study/jadiael/orgs/MainActivity.kt
+++ app/src/main/java/study/jadiael/orgs/MainActivity.kt
@@ -2,17 +2,12 @@ package study.jadiael.orgs
 
 import android.app.Activity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState);
-        val view = TextView(this);
-        view.setText("Jadiael Bonitão, Hello World!");
-        setContentView(view);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 
 }

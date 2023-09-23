91967731ca03ad2ceada5ae4fbc62aedec593f4c
jadiaelmonteiro <jadiael.monteiro@gmail.com>
2023-09-23 11:32:33 -0300

    Process of bind in the textView

--- app/src/main/java/study/jadiael/orgs/MainActivity.kt
+++ app/src/main/java/study/jadiael/orgs/MainActivity.kt
@@ -2,12 +2,24 @@ package study.jadiael.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Bananas, maçã e outras frutas"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
+
+
     }
 
 }
